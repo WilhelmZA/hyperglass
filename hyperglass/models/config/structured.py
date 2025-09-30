6f2a4f4b6ca9d0657787ae2ae5fba979654cb724
Wilhelm Schonfeldt <wilhelms@insight.co.za>
2025-09-30 12:12:34 +0200

    refactor: remove unused 'enabled' attribute from StructuredIpEnrichment class

--- hyperglass/models/config/structured.py
+++ hyperglass/models/config/structured.py
@@ -40,8 +40,6 @@ class StructuredRpki(HyperglassModel):
 
 class StructuredIpEnrichment(HyperglassModel):
     """Control IP enrichment for structured data responses."""
-
-    enabled: bool = False
     cache_timeout: int = 86400  # 24 hours in seconds (minimum)
     enrich_traceroute: bool = True
 
